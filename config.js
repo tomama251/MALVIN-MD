81cb17d73b0da270a839366b88f064ce7b818c0d
Malvin King <malvink003@gmail.com>
2024-11-19 23:06:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "9aMDBLRb#nixx_85Xa_uy4-KHPXbrA5AZeDOq2F3wsh8_BAOOJyE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/uxnee2.jpg",
 SUDO_NB: process.env.SUDO_NB || "263780934873",
-MONGODB: process.env.MONGODB || " ",
+MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: "true"
