1e8447f3fb63079cf6016276bce21f446268352c
Malvin King <malvink003@gmail.com>
2024-11-19 23:05:52 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "9aMDBLRb#nixx_85Xa_uy4-KHPXbrA5AZeDOq2F3wsh8_BAOOJyE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/uxnee2.jpg",
 SUDO_NB: process.env.SUDO_NB || "263780934873",
+MONGODB: process.env.MONGODB || " ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: "true"
