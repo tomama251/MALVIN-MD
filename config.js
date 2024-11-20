96af7220da55f504f5aaab4789efd70b98a0aae4
Malvin King <malvink003@gmail.com>
2024-11-20 19:10:23 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9aMDBLRb#nixx_85Xa_uy4-KHPXbrA5AZeDOq2F3wsh8_BAOOJyE",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/uxnee2.jpg",
+SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
 SUDO_NB: process.env.SUDO_NB || "263780934873",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
